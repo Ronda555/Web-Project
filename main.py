c66e727bb1a081fa7a754b94a217f4bc7dc54f38
Ronda <Rondaw555@yandex.ru>
2026-05-13 20:57:28 +0300

    login-flask

--- main.py
+++ main.py
@@ -3,15 +3,22 @@ from data import db_session
 from data.users import User
 from forms.user import RegisterForm
 from forms.Login import LoginForm
+from flask_login import LoginManager, login_user, current_user
 
 
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'в разработке'
+
+    if current_user.is_authenticated:
+        return f'Привет, {current_user.name}'
+
+    return 'Вы не вошли'
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -71,9 +78,16 @@ def login():
                 message='Неверный пароль'
             )
 
+        login_user(user)
+
         return redirect('/')
 
     return render_template('login.html', form=form)
+
+@login_manager.user_loader
+def load_user(user_id):
+    session = db_session.create_session()
+    return session.query(User).get(user_id)
 
 def main():
     # создание базы данных
